87be07b5ad736f93d491e9f1db7d7ce81442e4a2
Noahkimtai <noahkimtai24@gmail.com>
2022-09-28 13:35:47 +0300

    listof transactions added

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Transaction() {
+// the transaction component will take a single transaction as a prop
+// it will then display  date, description,category and amount of the transaction
+function Transaction({transaction}) {
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{"your code here..."} this component gets loaded{transaction.date}</td>
+      <td>{"your code here..."}{transaction.description}</td>
+      <td>{"your code here..."}{transaction.category}</td>
+      <td>{"your code here..."}{transaction.amount}</td>
     </tr>
   );
 }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Transaction from "./Transaction";
 
+// TransactionList component should fetch all the transactions form db.json
+// for every transaction fetched Transaction component will be rendered 
+// It passes transaction down to Transaction component
 function TransactionsList() {
+  // create state to hold the list of transactions
+  const [transactionList, setTransactionList] = useState([]);
+  // fetch list of transactions as a side effect of the page load
+  useEffect(() =>{
+    fetch('http://localhost:3002/transactions').then(res => res.json())
+    .then(data => setTransactionList(data))
+  },[]);
+
+  //create Transaction component for every 
+  const transactionRow = transactionList.map((transactionEl) =><Transaction transaction ={transactionEl}/>)
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -20,6 +33,7 @@ function TransactionsList() {
           </th>
         </tr>
         {/* render a list of <Transaction> components here */}
+        {transactionRow}
       </tbody>
     </table>
   );
